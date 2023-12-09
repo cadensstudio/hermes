584ea2dab58ca0c0ca97031743475af26db1513a
Caden <cadenkulp@gmail.com>
2023-12-09 15:01:20 -0800

    feat: print css rules

--- cmd/get.go
+++ cmd/get.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -26,8 +27,10 @@ type Axes struct {
 // extract font file path url from Google Fonts API JSON response
 type Font struct {
 	Items []struct {
-		Files map[string]string `json:"files"`
-		Axes  []*Axes           `json:"axes,omitempty"`
+		Family   string            `json:"family"`
+		Variants []string          `json:"variants"`
+		Files    map[string]string `json:"files"`
+		Axes     []*Axes           `json:"axes,omitempty"`
 	} `json:"items"`
 }
 
@@ -36,21 +39,20 @@ var getCmd = &cobra.Command{
 	Use:   "get [FONT FAMILY] [FLAGS]",
 	Short: "Download web-optimized font files for a specified font family.",
 	Long: `
-Downloads the specified font family in the WOFF2 format.
+Download the specified font family in WOFF2 format.
 By default, if a single variable format is available, it will be downloaded;
-otherwise, each individual font weight file will be downloaded.`,
+otherwise, each individual font weight file will be downloaded in the current directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			cmd.Help()
 		} else {
 			fontFamily := args[0]
 			parsedFontFamily := parseFontFamily(fontFamily)
-			fontUrl := getFontUrl(parsedFontFamily)
-			if len(fontUrl) >= 1 {
-				donwloadFont(parsedFontFamily, fontUrl)
+			fontResponse := getFontUrl(parsedFontFamily)
+			if len(fontResponse.Items) >= 1 {
+				donwloadFont(fontResponse)
 			}
 		}
-
 	},
 }
 
@@ -68,7 +70,23 @@ func init() {
 	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func getFontUrl(fontFamily string) (fontFiles map[string]string) {
+func parseFontFamily(fontFamily string) (parsedFontFamily string) {
+	// convert font input to lowercase
+	fontFamily = cases.Lower(language.Und).String(fontFamily)
+	// convert first letter of each word to uppercase
+	fontFamily = cases.Title(language.Und).String(fontFamily)
+	// replace spaces with + for url formatting
+	for _, char := range fontFamily {
+		if char == ' ' {
+			parsedFontFamily += "+"
+		} else {
+			parsedFontFamily += string(char)
+		}
+	}
+	return parsedFontFamily
+}
+
+func getFontUrl(fontFamily string) (fontResponse Font) {
 	key := viper.Get("GFONTS_KEY")
 	url := "https://www.googleapis.com/webfonts/v1/webfonts?key=" + fmt.Sprint(key) + "&family=" + fontFamily + "&capability=WOFF2&capability=VF"
 
@@ -90,33 +108,17 @@ func getFontUrl(fontFamily string) (fontFiles map[string]string) {
 		}
 
 		// parse the response body into the Font object struct
-		var font Font
-		err = json.Unmarshal(body, &font)
-
+		var fontResponse Font
+		err = json.Unmarshal(body, &fontResponse)
 		if err != nil {
 			fmt.Println("Error parsing json response", err)
 		}
-
-		// for _, axis := range font.Items[0].Axes {
-		// 	fmt.Printf("Tag: %s\n", axis.Tag)
-		// 	fmt.Printf("Start: %d\n", axis.Start)
-		// 	fmt.Printf("End: %d\n", axis.End)
-		// }
-		if len(font.Items[0].Axes) == 0 {
-			fmt.Println("Variable font file not available")
-			fmt.Println("Proceeding to download font files individually...")
-		} else {
-			fmt.Println("Variable font file found!")
-			fmt.Println("Proceeding to download variable font file(s)...")
-		}
-		// grab all font files from the json response
-		fontFiles = font.Items[0].Files
-		return fontFiles
+		return fontResponse
 	} else if res.StatusCode == 400 {
-		fmt.Println("400: Could not complete request")
+		fmt.Println("400 Error: Could not complete request")
 		return
 	} else if res.StatusCode == 500 {
-		fmt.Println("500: Could not find specified font:", fontFamily)
+		fmt.Println("500 Error: Could not find specified font:", fontFamily)
 		return
 	} else {
 		fmt.Println("An unexpected error occured")
@@ -124,7 +126,24 @@ func getFontUrl(fontFamily string) (fontFiles map[string]string) {
 	}
 }
 
-func donwloadFont(fontFamily string, fontFiles map[string]string) {
+func donwloadFont(fontResponse Font) {
+	var hasVariable bool
+	fontFamily := fontResponse.Items[0].Family
+	fontFiles := fontResponse.Items[0].Files
+
+	if len(fontResponse.Items[0].Axes) == 0 {
+		hasVariable = false
+		fmt.Println("Variable font file not available.")
+		fmt.Println("Downloading font files individually...")
+	} else {
+		hasVariable = true
+		if len(fontFiles) == 1 {
+			fmt.Println("Downloading variable font file...")
+		} else {
+			fmt.Println("Downloading variable font file(s)...")
+		}
+	}
+
 	for variant, url := range fontFiles {
 		// Make the GET request for each variant
 		res, err := http.Get(url)
@@ -149,23 +168,56 @@ func donwloadFont(fontFamily string, fontFiles map[string]string) {
 			fmt.Println(err)
 			continue // Skip to the next variant if an error occurs
 		}
-
 		fmt.Printf("%s successfully downloaded!\n", filepath)
 	}
+	fmt.Println("\nNext steps: Copy the following CSS rules wherever you would like to use your font!")
+	printCssStrings(fontResponse, hasVariable)
 }
 
-func parseFontFamily(fontFamily string) (parsedFontFamily string) {
-	// convert font input to lowercase
-	fontFamily = cases.Lower(language.Und).String(fontFamily)
-	// convert first letter of each word to uppercase
-	fontFamily = cases.Title(language.Und).String(fontFamily)
-	// replace spaces with + for url formatting
-	for _, char := range fontFamily {
-		if char == ' ' {
-			parsedFontFamily += "+"
-		} else {
-			parsedFontFamily += string(char)
+func printCssStrings(fontResponse Font, hasVariable bool) {
+	var newCssString string
+
+	if hasVariable {
+		var startWeight string
+		var endWeight string
+		for _, axis := range fontResponse.Items[0].Axes {
+			if axis.Tag == "wght" {
+				startWeight = strconv.Itoa(axis.Start)
+				endWeight = strconv.Itoa(axis.End)
+			}
+		}
+		for _, variant := range fontResponse.Items[0].Variants {
+			newCssString = `
+@font-face {
+  font-family: '` + fontResponse.Items[0].Family + `';
+  font-style: ` + variant + `;
+  font-weight: ` + startWeight + `-` + endWeight + `;
+  src: url('./` + fontResponse.Items[0].Family + `_` + variant + `.woff2') format('woff2');
+}`
+			fmt.Println(newCssString)
+		}
+	} else {
+		for _, variant := range fontResponse.Items[0].Variants {
+			var fontStyle string
+			var fontWeight string
+			if variant == "regular" || variant == "italic" {
+				fontStyle = variant
+				fontWeight = "400"
+			} else {
+				fontStyle = variant[3:]
+				if len(fontStyle) == 0 {
+					fontStyle = "regular"
+				}
+				fontWeight = variant[:3]
+			}
+			newCssString = `
+@font-face {
+  font-family: '` + fontResponse.Items[0].Family + `';
+  font-style: ` + fontStyle + `;
+  font-weight: ` + fontWeight + `;
+  src: url('./` + fontResponse.Items[0].Family + `_` + variant + `.woff2') format('woff2');
+}`
+			fmt.Println(newCssString)
 		}
 	}
-	return parsedFontFamily
 }
